eb8e7abf5af4c79148f4639a84906a396e89d766
Nassim Guenez <gueneznassim@gmail.com>
2018-02-21 10:56:36 +0100

    first commit

---
+++ app/src/main/java/com/example/android/ngplayer/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.android.ngplayer;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seek_bar);
+        mediaPlayer = MediaPlayer.create(this, R.raw.sob_rashrash);
+        seekBar.setMax(mediaPlayer.getDuration());
+        final Button playPauseButton = findViewById(R.id.play_pause_button);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!mediaPlayer.isPlaying()) {
+                    playPauseButton.setBackgroundResource(R.drawable.pause);
+                    mediaPlayer.start();
+                } else {
+                    playPauseButton.setBackgroundResource(R.drawable.play);
+                    mediaPlayer.pause();
+                }
+
+            }
+        });
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
+                if (input) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+}
